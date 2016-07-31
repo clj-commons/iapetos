5a05478f221488caa95bad122b7a8eca6fc5468d
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-07-31 16:08:55 +0200

    add tests for summary.

---
+++ test/iapetos/core/summary_test.clj
@@ -0,0 +1,90 @@
+(ns iapetos.core.summary-test
+  (:require [clojure.test.check
+             [generators :as gen]
+             [properties :as prop]
+             [clojure-test :refer [defspec]]]
+            [clojure.test :refer :all]
+            [iapetos.test.generators :as g]
+            [iapetos.core :as prometheus]))
+
+;; ## Summary w/o Labels
+
+(def gen-ops
+  (gen/vector
+    (gen/let [v (gen/double* {:infinite? false, :NaN? false})]
+      (let [amount (Math/abs v)
+            effect #(-> %
+                        (update :count inc)
+                        (update :sum   + amount))]
+        (gen/elements
+          [{:f      #(prometheus/observe %1 %2 amount)
+            :form   '(observe registry metric ~amount)
+            :effect effect}
+           {:f      #(prometheus/observe (%1 %2) amount)
+            :form   '(observe (registry metric) ~amount)
+            :effect effect}])))))
+
+(defspec t-summary 100
+  (prop/for-all
+    [metric g/metric
+     ops    gen-ops]
+    (let [registry (-> (prometheus/collector-registry)
+                       (prometheus/register
+                         (prometheus/summary metric)))
+          expected-value (reduce
+                           #((:effect %2) %1)
+                           {:count 0.0, :sum 0.0}
+                           ops)]
+      (doseq [{:keys [f]} ops]
+        (f registry metric))
+      (= expected-value (prometheus/value (registry metric))))))
+
+;; ## Summary w/ Labels
+
+(def labels
+  {:label "x"})
+
+(def gen-ops
+  (gen/vector
+    (gen/let [v (gen/double* {:infinite? false, :NaN? false})]
+      (let [amount (Math/abs v)
+            effect #(-> %
+                        (update :count inc)
+                        (update :sum   + amount))]
+        (gen/elements
+          [{:f      #(prometheus/observe %1 %2 labels amount)
+            :form   '(observe registry metric labels ~amount)
+            :effect effect}
+           {:f      #(prometheus/observe (%1 %2 labels) amount)
+            :form   '(observe (registry metric labels) ~amount)
+            :effect effect}])))))
+
+(defspec t-summary-with-labels 100
+  (prop/for-all
+    [metric g/metric
+     ops    gen-ops]
+    (let [registry (-> (prometheus/collector-registry)
+                       (prometheus/register
+                         (prometheus/summary metric {:labels (keys labels)})))
+          expected-value (reduce
+                           #((:effect %2) %1)
+                           {:count 0.0, :sum 0.0}
+                           ops)]
+      (doseq [{:keys [f]} ops]
+        (f registry metric))
+      (= expected-value (prometheus/value (registry metric labels))))))
+
+;; ## Summary Timer
+
+(deftest t-summary-timer
+  (let [metric :app/duration-seconds
+        registry (-> (prometheus/collector-registry)
+                     (prometheus/register
+                       (prometheus/summary metric {:lazy? true})))
+        start      (System/nanoTime)
+        stop-timer (prometheus/start-timer registry metric)
+        _          (do (Thread/sleep 50) (stop-timer))
+        delta      (/ (- (System/nanoTime) start) 1e9)
+        {:keys [sum count]} (prometheus/value (registry metric))]
+    (is (= count 1.0))
+    (is (<= 0.05 sum delta))))
