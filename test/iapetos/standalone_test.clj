f27c5beb17913901a8e5f84f929c0abfe020049a
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-08-08 18:04:58 +0200

    use registry generator for standalone server tests.

--- test/iapetos/standalone_test.clj
+++ test/iapetos/standalone_test.clj
@@ -1,5 +1,10 @@
 (ns iapetos.standalone-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test.check
+             [generators :as gen]
+             [properties :as prop]
+             [clojure-test :refer [defspec]]]
+            [clojure.test :refer :all]
+            [iapetos.test.generators :as g]
             [iapetos.core :as prometheus]
             [iapetos.export :as export]
             [iapetos.standalone :as standalone]
@@ -12,24 +17,30 @@
 
 (defn- fetch
   [request-method url]
-  (-> (http/request
-        {:method            request-method
-         :throw-exceptions? false
-         :url               url})
-      (deref)
-      ((juxt :status (comp slurp :body)))))
+  (try
+    (-> (http/request
+          {:method            request-method
+           :throw-exceptions? false
+           :url               url})
+        (deref)
+        ((juxt :status (comp slurp :body))))
+    (catch Throwable t
+      (println "exception when querying server:" t)
+      t)))
 
-(deftest t-standalone-server
-  (let [registry (-> (prometheus/collector-registry)
-                     (prometheus/register
-                       (prometheus/counter :app/runs-total))
-                     (prometheus/inc :app/runs-total))]
-    (with-open [_ (standalone/metrics-server
-                    registry
-                    {:port 65432
-                     :path "/prometheus-metrics"})]
-      (is (= [200 (export/text-format registry)] (fetch :get metrics-url)))
-      (is (= [405 "Method not allowed: POST" (fetch :post metrics-url)]))
-      (is (= [404 (str "Not found: " path "/_info")]
-             (fetch :get (str metrics-url "/_info"))))
-      (is (= [404 "Not found: /"] (fetch :get metrics-host))))))
+(defspec t-standalone-server 5
+  (prop/for-all
+    [registry-fn (g/registry-fn)]
+    (let [registry (-> (registry-fn)
+                       (prometheus/register
+                         (prometheus/counter :app/runs-total))
+                       (prometheus/inc :app/runs-total))]
+      (with-open [_ (standalone/metrics-server
+                      registry
+                      {:port 65432
+                       :path "/prometheus-metrics"})]
+        (and (= [200 (export/text-format registry)] (fetch :get metrics-url))
+             (= [405 "Method not allowed: POST" (fetch :post metrics-url)])
+             (= [404 (str "Not found: " path "/_info")]
+                (fetch :get (str metrics-url "/_info")))
+             (= [404 "Not found: /"] (fetch :get metrics-host)))))))
