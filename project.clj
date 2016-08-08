f456e4604e91d78cabb21ea8d565a9e95d7250fc
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-08-08 18:59:14 +0200

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject iapetos "0.1.0"
+(defproject iapetos "0.1.1-SNAPSHOT"
   :description "A Clojure Prometheus Client"
   :url "https://github.com/xsc/iapetos"
   :license {:name "MIT License"
