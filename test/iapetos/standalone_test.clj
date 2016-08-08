9bd2ff1cfd6cc5d606589975d2387742a0aeae1e
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-08-08 18:54:08 +0200

    adjust standalone server tests to use random port.

--- test/iapetos/standalone_test.clj
+++ test/iapetos/standalone_test.clj
@@ -10,18 +10,15 @@
             [iapetos.standalone :as standalone]
             [aleph.http :as http]))
 
-(def ^:private port 65432)
 (def ^:private path "/prometheus-metrics")
-(def ^:private metrics-host (str "http://localhost:" port))
-(def ^:private metrics-url (str metrics-host path))
 
 (defn- fetch
-  [request-method url]
+  [{:keys [port]} request-method path]
   (try
     (-> (http/request
           {:method            request-method
            :throw-exceptions? false
-           :url               url})
+           :url               (str "http://localhost:" port path)})
         (deref)
         ((juxt :status (comp slurp :body))))
     (catch Throwable t
@@ -37,12 +34,13 @@
                        (prometheus/inc :app/runs-total))]
       (with-open [server (standalone/metrics-server
                            registry
-                           {:port 65432
+                           {:port 0
                             :path "/prometheus-metrics"})]
-        (Thread/sleep 100)
-        (and (= port (:port server))
-             (= [200 (export/text-format registry)] (fetch :get metrics-url))
-             (= [405 "Method not allowed: POST" (fetch :post metrics-url)])
+        (and (= [200 (export/text-format registry)]
+                (fetch server :get path))
+             (= [405 "Method not allowed: POST"]
+                (fetch server :post path))
              (= [404 (str "Not found: " path "/_info")]
-                (fetch :get (str metrics-url "/_info")))
-             (= [404 "Not found: /"] (fetch :get metrics-host)))))))
+                (fetch server :get (str path "/_info")))
+             (= [404 "Not found: /"]
+                (fetch server :get "")))))))
