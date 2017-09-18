d8bd6569787f6d1b16101d08dfd1c41f7a1a8563
Yannick Scherer <yannick.scherer@futurice.com>
2017-09-18 17:22:34 +0200

    Version 0.1.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject iapetos "0.1.7"
+(defproject iapetos "0.1.8-SNAPSHOT"
   :description "A Clojure Prometheus Client"
   :url "https://github.com/xsc/iapetos"
   :license {:name "MIT License"
