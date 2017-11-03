67ee33b91b29c0c2548d1544824b62596852ba62
Yannick Scherer <yannick.scherer@futurice.com>
2017-11-03 11:21:48 +0100

    add basic test for JVM collectors.

---
+++ test/iapetos/collector/jvm_test.clj
@@ -0,0 +1,19 @@
+(ns iapetos.collector.jvm-test
+  (:require [clojure.test.check
+             [generators :as gen]
+             [properties :as prop]
+             [clojure-test :refer [defspec]]]
+            [clojure.test :refer :all]
+            [iapetos.test.generators :as g]
+            [iapetos.core :as prometheus]
+            [iapetos.collector.jvm :as jvm]))
+
+(defspec t-jvm-collectors 10
+  (prop/for-all
+    [registry-fn (g/registry-fn)]
+    (let [registry (-> (registry-fn)
+                       (jvm/initialize))]
+      (and (registry :iapetos-internal/jvm-standard)
+           (registry :iapetos-internal/jvm-gc)
+           (registry :iapetos-internal/jvm-memory-pools)
+           (registry :iapetos-internal/jvm-threads)))))
