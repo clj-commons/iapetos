fc5a9a8275f37fa1ce285ca16c254ffa8bd08124
Yannick Scherer <yannick.scherer@futurice.com>
2018-02-22 11:22:59 +0100

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
             :year 2016
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [io.prometheus/simpleclient "0.1.0"]
-                 [io.prometheus/simpleclient_common "0.1.0"]
-                 [io.prometheus/simpleclient_pushgateway "0.1.0"]
-                 [io.prometheus/simpleclient_hotspot "0.1.0" :scope "provided"]]
+                 [io.prometheus/simpleclient "0.2.0"]
+                 [io.prometheus/simpleclient_common "0.2.0"]
+                 [io.prometheus/simpleclient_pushgateway "0.2.0"]
+                 [io.prometheus/simpleclient_hotspot "0.2.0" :scope "provided"]]
   :profiles {:dev
              {:dependencies [[org.clojure/test.check "0.9.0"]
                              [aleph "0.4.4"]]
@@ -27,8 +27,8 @@
                                    #"^iapetos\.collector\..+"]}}
              :coverage
              {:plugins [[lein-cloverage "1.0.9"]]
-              :dependencies [[org.clojure/tools.reader "1.1.0"]
-                             [riddley "0.1.14"]]}}
+              :dependencies [[org.clojure/tools.reader "1.2.2"]
+                             [riddley "0.1.15"]]}}
   :aliases {"codox" ["with-profile" "+codox" "codox"]
             "codecov" ["with-profile" "+coverage" "cloverage" "--codecov"]}
   :pedantic? :abort)
